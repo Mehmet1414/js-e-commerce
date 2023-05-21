37693d2d5a57681e0843af8fc640cea96486e43f
Mehmet Aydin <110032695+Mehmet1414@users.noreply.github.com>
2023-05-21 19:00:03 +0200

    start-index.html

--- JS/login.js
+++ JS/login.js
@@ -5,6 +5,7 @@ const passwordInput = document.querySelector("#password");
 const buttonRegister = document.querySelector(".btn-register");
 const errorUserName = document.querySelector("#errorUserName");
 const errorPassword = document.querySelector("#errorPassword");
+const errorUser = document.querySelector("#errorUser")
 
 document.addEventListener("keydown", function (e) {
   if (e.key === "Enter") {
@@ -32,8 +33,24 @@ form.addEventListener("submit", function (e) {
     errorPassword.style.visibility = "hidden";
   }
 
+  console.log(users)
+
+  let isLoggedIn= "false"
+
+  users.find((item,i)=>{
+
+    if (item.username===userNameInput.value && item.password===passwordInput.value) {
+         
+        //console.log("giris yapildi")
+        window.open("index.html")
+    }else{
+        errorUser.style.visibility="visible"
+    }
+  })
+
   userNameInput.value = "";
   passwordInput.value = "";
+
 });
 
 buttonRegister.addEventListener("click", function () {
