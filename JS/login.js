5b3ea032500ede60de6761b859ab905570f6ebde
Mehmet Aydin <110032695+Mehmet1414@users.noreply.github.com>
2023-05-21 15:10:55 +0200

    login js tamamlandi

--- JS/login.js
+++ JS/login.js
@@ -2,24 +2,40 @@ const form = document.querySelector(".login-form");
 const userNameInput = document.getElementById("username");
 const passwordInput = document.querySelector("#password");
 
-const buttonRegister = document.querySelector(".btn-register")
+const buttonRegister = document.querySelector(".btn-register");
+const errorUserName = document.querySelector("#errorUserName");
+const errorPassword = document.querySelector("#errorPassword");
 
-document.addEventListener("keydown",function(e){
-    if (e.key==="Enter") {
-        
-    }
-})
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+  }
+});
 
 let users = JSON.parse(localStorage.getItem("users")) || [];
-console.log(users)
+console.log(users);
 
-form.addEventListener('submit',function(e){
-    e.preventDefault();
-    
-    
-})
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-buttonRegister.addEventListener("click",function(){
-    window.open("register.html", "_blank");
+  if (userNameInput.value === "") {
+    errorUserName.style.visibility = "visible";
+    userNameInput.focus();
+    return;
+  } else {
+    errorUserName.style.visibility = "hidden";
+  }
+  if (passwordInput.value === "") {
+    errorPassword.style.visibility = "visible";
+    passwordInput.focus();
+    return;
+  } else {
+    errorPassword.style.visibility = "hidden";
+  }
 
-})
+  userNameInput.value = "";
+  passwordInput.value = "";
+});
+
+buttonRegister.addEventListener("click", function () {
+  window.open("register.html", "_blank");
+});
